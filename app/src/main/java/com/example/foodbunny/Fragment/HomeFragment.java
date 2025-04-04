94d0bc843225e558b3a8e426aa31c9b7c1de757f
saksham449 <mittalsaksham967@gmail.com>
2025-04-04 17:53:41 +0530

    banner(sliding) added

--- app/src/main/java/com/example/foodbunny/Fragment/HomeFragment.java
+++ app/src/main/java/com/example/foodbunny/Fragment/HomeFragment.java
@@ -1,66 +1,55 @@
 package com.example.foodbunny.Fragment;
 
 import android.os.Bundle;
-
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import com.example.foodbunny.R;
+import com.example.foodbunny.databinding.FragmentHomeBinding;
+import com.denzcoskun.imageslider.ImageSlider;
+import com.denzcoskun.imageslider.models.SlideModel;
+import com.denzcoskun.imageslider.constants.ScaleTypes;
+import java.util.ArrayList;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link HomeFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class HomeFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private FragmentHomeBinding binding; // View binding object for this fragment
 
     public HomeFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static HomeFragment newInstance(String param1, String param2) {
-        HomeFragment fragment = new HomeFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        // Inflate the layout using ViewBinding
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        return binding.getRoot(); // Return the root view
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // Creating an ArrayList to store slide images
+        ArrayList<SlideModel> imageList = new ArrayList<>();
+
+        // Adding images to the slider list
+        imageList.add(new SlideModel(R.drawable.banner1, ScaleTypes.FIT));
+        imageList.add(new SlideModel(R.drawable.banner2, ScaleTypes.FIT));
+        imageList.add(new SlideModel(R.drawable.banner3, ScaleTypes.FIT));
+
+        // Getting reference to the ImageSlider using ViewBinding
+        ImageSlider imageSlider = binding.imageSlider;
+
+        // Setting the images in the ImageSlider
+        imageSlider.setImageList(imageList);
     }
 }
